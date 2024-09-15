9b94db2214a6a87da64c96aeed1e77d8484c47e4
fernandoberbel <fberbel1000@gmail.com>
2024-09-15 13:56:01 -0300

    feat: add performance charts generation

--- src/market-report.py
+++ src/market-report.py
@@ -14,4 +14,23 @@ dados_mercado = dados_mercado.dropna()
 # renomear as colunas
 dados_mercado.columns = ["DOLAR", "IBOVESPA", "S&P500"]
 
-dados_mercado
+# escolhendo tema cyberpunk para os gráficos
+# plt.style.use("cyberpunk")
+# plotando gráfico do ibovespa
+plt.plot(dados_mercado["IBOVESPA"])
+# dando título para o gráfico
+plt.title("IBOVESPA")
+# plotar o gráfico na tela
+plt.show()
+# salvar a imagem do gráfico no computador
+plt.savefig("ibovespa.png")
+
+plt.plot(dados_mercado["DOLAR"])
+plt.title("DOLAR")
+plt.show()
+plt.savefig("dolar.png")
+
+plt.plot(dados_mercado["S&P500"])
+plt.title("S&P500")
+plt.show()
+plt.savefig("sp500.png")
