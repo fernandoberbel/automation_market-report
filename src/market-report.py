6edcfd20391af5486fb54402bc2e1e38558e9aad
fernandoberbel <fberbel1000@gmail.com>
2024-09-15 00:02:16 -0300

    feat: add market data

--- src/market-report.py
+++ src/market-report.py
@@ -1,9 +1,17 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-import mplcyberpunk
+# import mplcyberpunk
 
 tickers = ["^BVSP", "^GSPC", "BRL=X"]
 
+# pegar as cotações
 dados_mercado = yf.download(tickers, period="6mo")
+# pegar apenas os valores de fechamento do mercado
+dados_mercado = dados_mercado["Adj Close"]
+# tratar os dados eliminando dados faltantes
+dados_mercado = dados_mercado.dropna()
+# renomear as colunas
+dados_mercado.columns = ["DOLAR", "IBOVESPA", "S&P500"]
+
 dados_mercado
