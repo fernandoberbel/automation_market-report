70ef541a1df2d71f5a0403cceda0f1249cca498c
fernandoberbel <fberbel1000@gmail.com>
2024-09-15 15:30:40 -0300

    feat: add daily return of data and e-mail report

--- src/market-report.py
+++ src/market-report.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 # import mplcyberpunk
+import win32com.client as win32
 
 tickers = ["^BVSP", "^GSPC", "BRL=X"]
 
@@ -34,3 +35,46 @@ plt.plot(dados_mercado["S&P500"])
 plt.title("S&P500")
 plt.show()
 plt.savefig("sp500.png")
+
+# cálcular retornos diários
+# colocando em porcentagem
+retornos_diarios = dados_mercado.pct_change()
+retorno_dolar = retornos_diarios["DOLAR"].iloc[-1]
+retorno_ibovespa = retornos_diarios["IBOVESPA"].iloc[-1]
+retorno_sp = retornos_diarios["S&P500"].iloc[-1]
+
+# covertendo em porcentagem
+retorno_dolar = str(round(retorno_dolar * 100, 2)) + "%"
+retorno_ibovespa = str(round(retorno_ibovespa * 100, 2)) + "%"
+retorno_sp = str(round(retorno_sp * 100, 2)) + "%"
+
+# enviar os dados por e-mail
+# abrir aplicação de e-mail do outlook
+outlook = win32.Dispatch("outlook.application")
+# criar e-mail
+email = outlook.CreateItem(0)
+# configurar e-mail que será enviado
+email.To = "fernando_berbel@msn.com"
+email.Subject = "Relatório de Mercado"
+email.Body = f''' Prezado diretor, segue o relatório de mercado:
+
+* O Ibovespa teve o retorno de {retorno_ibovespa}.
+* O Dólar teve o retorno de {retorno_dolar}.
+* O S&P500 teve o retorno de {retorno_sp}.
+
+Segue em anexo a peformance dos ativos nos últimos 6 meses.
+
+Att,
+Melhor estagiário do mundo
+'''
+# inserindo os anexos
+# separando os arquivos
+anexo_ibovespa = r"C:\Users\Fernando\Documents\Development\Varos\Aulas\Projeto 1\relatório-automatizado-de-mercado\src\ibovespa.png"
+anexo_dolar = r"C:\Users\Fernando\Documents\Development\Varos\Aulas\Projeto 1\relatório-automatizado-de-mercado\src\dolar.png"
+anexo_sp = r"C:\Users\Fernando\Documents\Development\Varos\Aulas\Projeto 1\relatório-automatizado-de-mercado\src\sp500.png"
+# adicionando os anexos
+email.Attachments.Add(anexo_ibovespa)
+email.Attachments.Add(anexo_dolar)
+email.Attachments.Add(anexo_sp)
+# enviar e-mail
+email.Send()
